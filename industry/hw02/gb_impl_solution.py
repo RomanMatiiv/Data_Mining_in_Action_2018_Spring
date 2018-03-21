0d1bb9720c13096a420bd8f0786bc26ddc4b3199
RomanMatiiv <matiiv.r.a@gmail.com>
2018-03-21 17:09:23 +0300

    fix error

--- industry/hw02/gb_impl_solution.py
+++ industry/hw02/gb_impl_solution.py
@@ -1,10 +1,12 @@
 from sklearn.base import BaseEstimator
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
 import numpy as np
 
 
-TREE_PARAMS_DICT = {'max_depth': 2}
-TAU = 0.05
+TREE_PARAMS_DICT = {'max_depth': 8, 'max_features': 3, 'random_state': 42}
+TAU = 0.07
 
 
 class SimpleGB(BaseEstimator):
@@ -14,9 +16,11 @@ class SimpleGB(BaseEstimator):
         self.tau = tau
         
     def fit(self, X_data, y_data):
-        self.base_algo = DecisionTreeRegressor(**self.tree_params_dict).fit(X_data, y_data)
+        self.base_algo = LogisticRegression(C=0.0005).fit(X_data, y_data)
         self.estimators = []
-        curr_pred = self.base_algo.predict(X_data)
+        # p = 1 / (1 + exp(-a)) => a = - ln (1 / p - 1) = ln(p / (1 - p))
+        curr_pred = - np.log(1. / self.base_algo.predict_proba(X_data)[:, 1] - 1)
+
         for iter_num in range(self.iters):
             # y это 0 или 1
             # a - сырое предсказание
@@ -25,7 +29,7 @@ class SimpleGB(BaseEstimator):
             # log loss это (y log f(a) + (1 - y) log(1 - f(a)))
 
             # d/da (y log f(a) + (1 - y) log(1 - f(a))) = f'(a) (y/f(a) - (1 - y) / (1 - f(a)))
-            fa = 1. / (1 + exp(-curr_pred))
+            fa = 1. / (1 + np.exp(-curr_pred))
             grad = - fa * (1. - fa) * (y_data / fa - (1. - y_data) / (1. - fa))
             algo = DecisionTreeRegressor(**self.tree_params_dict).fit(X_data, - grad)
             self.estimators.append(algo)
@@ -33,7 +37,7 @@ class SimpleGB(BaseEstimator):
         return self
     
     def predict(self, X_data):
-        res = self.base_algo.predict(X_data)
+        res = - np.log(1. / self.base_algo.predict_proba(X_data)[:, 1] - 1)
         for estimator in self.estimators:
             res += self.tau * estimator.predict(X_data)
-        return res > 0.5
+        return res > 0.1 # этот порог можно варировать с целью повышения метрики
